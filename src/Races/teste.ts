20b58b85757c58c955a568ab655e891a3b33797d
Arthur Pelegrino <arthurpelegrino97@gmail.com>
2023-05-11 17:03:37 -0300

    Criando a classe abstrata Archertypes

---
+++ src/Races/teste.ts
@@ -0,0 +1,21 @@
+// import { Energy, EnergyType } from '../Energy';
+
+// class TestEnergy implements Energy {
+//   amount: number;
+//   type_: EnergyType;
+//   constructor(amount: number, type_: EnergyType) {
+//     this.amount = amount;
+//     this.type_ = type_; 
+//   }
+// }
+
+// const novoTeste = new TestEnergy(45, 'mana');
+
+// console.log(novoTeste);
+
+// import Dwarf from './Dwarf';
+
+// const anao1 = new Dwarf('zangado', 80);
+// const anao2 = new Dwarf('zangado2', 70);
+// console.log(anao2);
+// console.log(anao1);
