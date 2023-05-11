fac0fa6920412a458cb007b1af4a4cc3e438bf07
Arthur Pelegrino <arthurpelegrino97@gmail.com>
2023-05-11 17:39:34 -0300

    Requisito 6, criando a interface Fighter

--- src/Fighter/Fighter.ts
+++ src/Fighter/Fighter.ts
@@ -1,14 +1,14 @@
-// import Energy from '../Energy';
+import Energy from '../Energy';
 
-// export default interface Fighter {
-//   lifePoints: number;
-//   strength: number;
-//   defense: number;
-//   energy?: Energy;
+export default interface Fighter {
+  lifePoints: number;
+  strength: number;
+  defense: number;
+  energy?: Energy;
 
-//   attack(enemy: Fighter): void
-//   special?(enemy: Fighter): void
-//   levelUp(): void
-//   receiveDamage(attackPoints:number): number
+  attack(enemy: Fighter): void
+  special?(enemy: Fighter): void
+  levelUp(): void
+  receiveDamage(attackPoints:number): number
 
-// }
+}
